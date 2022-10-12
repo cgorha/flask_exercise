555dd11a93902af102aaceb638653554c779db53
coaleyyy <112022464+coaleyyy@users.noreply.github.com>
2022-10-12 18:12:30 -0400

    hi

---
+++ app.py
@@ -0,0 +1,66 @@
+from datetime import date
+from urllib import request
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+global studentOrganisationDetails
+# Assign default 5 values to studentOrganisationDetails for Application  3.
+studentOrganisationDetails = 'Hi'
+studentOrganisationDetails = 'yo'
+studentOrganisationDetails = 'lel'
+studentOrganisationDetails = 'nope'
+studentOrganisationDetails = 'define'
+
+@app.get('/')
+def index():
+    # Complete this function to get current date and time assign this value to currentDate, display this data on index.html
+    currentDate = datetime.datetime.now()
+    return render_template('index.html', currentDate=currentDate)
+
+
+@app.get('/calculate')
+def displayNumberPage():
+    # Complete this function to display form.html page
+    return render_template('form.html')
+
+
+@app.route('/calculate', methods=['POST'])
+def checkNumber():
+    # Get Number from form and display message according to number
+    # Display "Number {Number} is even" if given number is even on result.html page
+    # Display "Number {Number} is odd" if given number is odd on result.html page
+    # Display "No number provided" if value is null or blank on result.html page
+    # Display "Provided input is not an integer!" if value is not a number on result.html page
+    global number
+    
+    number = request.args['number']
+    try:
+        if int(number)%2 == 0:
+            msg = 'Number {Number} is even'
+        elif int(number)%2 !=0:
+            msg = 'Number {Number} is odd'
+        elif int(number)%1 !=0:
+            msg = "Provided input is not an integer!"
+    except:
+         msg = 'No number provided'
+
+
+    # Write your to code here to check whether number is even or odd and render result.html page
+
+
+@app.get('/addStudentOrganisation')
+def displayStudentForm():
+    # Complete this function to display studentFrom.html page
+    return render_template('studentForm.html')
+
+
+@app.route('/addStudentOrganisation', methods=['POST'])
+def displayRegistrationPage():
+    # Get student name and organisation from form.
+    studentName = request.form['name']
+    studentName = studentOrganisationDetails
+
+    # Append this value to studentOrganisationDetails
+
+    # Display studentDetails.html with all students and organisations
